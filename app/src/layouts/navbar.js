49cb1b3a519b24500623e1bb322907b35b3fe708
Stephanie Quon <stephanie.quon@alumni.ubc.ca>
2023-10-25 12:23:06 -0700

    remove footer

--- app/src/layouts/navbar.js
+++ app/src/layouts/navbar.js
@@ -300,30 +300,7 @@ export default function AppNavBar() {
                     {/* BH TODO: Delete this "dashboard" route once the new SNV page is implemented */}
                     <Route path="/dashboard" exact element={ <Dashboard/> } />
                 </Routes>
-                
-                <Box sx={{ position: 'fixed', bottom: 0, width: '100%', bgcolor: '#0F3057' }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
-                        <img src="/ubclogo.jpg" alt="Variome Logo" style={{ width: 200, marginRight: 10, marginLeft: 100 }} />
-                        <Typography variant="h5" component="div" sx={{ color: 'white', marginRight: '30px', marginLeft: '40px' }}>
-                            Variome
-                        </Typography>
-                        <Typography variant="h5" component="div" sx={{ color: 'white', marginRight: '30px' }}>
-                        <Link href="/about" color="inherit">
-                            About
-                        </Link>
-                        </Typography>
-                        <Typography variant="h5" component="div" sx={{ color: 'white', marginRight: '30px' }}>
-                        <Link href="/terms" color="inherit">
-                            Terms of Use
-                        </Link>
-                        </Typography>
-                        <Typography variant="h5" component="div" sx={{ color: 'white' }}>
-                        <Link href="/contact" color="inherit">
-                            Contact
-                        </Link>
-                        </Typography>
-                    </Box>
-                </Box>
+            
 
             </Main>
         </Box>
