2e86c123d3bc4dac349ee3d1ccc5736686ca8914
Brad W <bwinter@cmmt.ubc.ca>
2024-03-26 14:19:18 -0700

    frontend changes for new gene-organized annotation format

--- app/src/components/Annotations.js
+++ app/src/components/Annotations.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,33 +10,74 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import _ from 'lodash';
 
+export default function Annotations({ variantAnnotations, gene, filter }) {
 
-export default function Annotations({variantAnnotations}) {
+    function TranscriptsAnnotationsTable({ transcripts: filteredTranscripts, gene }) {
 
-    const [selectedTranscript, setSelectedTranscript] = useState('Ensembl');
+        var impactColors = {
+            'HIGH': 'red',
+            'MODERATE': 'orange',
+            'LOW': 'yellow',
+            'MODIFIER': 'green'
+        }
+        filteredTranscripts = _.filter(filteredTranscripts, filter)
 
-    const handleTranscriptChange = (event) => {
-        setSelectedTranscript(event.target.value);
-    };
+        var columns = _.without(_.keys(filteredTranscripts[0]), 'gene', 'database');
+        return (
+            <Box sx={{ overflowX: 'scroll', maxWidth: '100vw' }} >
+
+                <Table aria-label={"annotations for gene " + gene}  >
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column, index) => (
+                                <TableCell key={index} align="center" sx={{ borderBottom: 'none', fontWeight: 'bold', padding: '0em 0.5em', textTransform: 'capitalize' }}>{column}</TableCell>
+                            ))}
+                        </TableRow>
+
+                    </TableHead>
+                    <TableBody>
+                        {filteredTranscripts.map((transcript, index) => {
+                            var color = impactColors[_.get(transcript,'impact')];
+                            var coloredDot = <></>
+                            if (color){
+                                coloredDot = <span style={{ cursor:'default',fontSize:'8px', verticalAlign:'middle', padding:'0.5em', color }}>&#11044;</span>
+                            }
+                            return (
+                                <TableRow key={index}>
+                                    {columns.map((column, index) => (
+                                        <TableCell align="center" key={index}>
+                                            {column === 'consequence'? coloredDot : ''}
+                                            {transcript[column]}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </Box>
+        );
+    }
 
     return (
-        <React.Fragment>
-            <Card>
-                <CardContent>
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <Typography variant="h4" sx={{ fontWeight: 'light', paddingBottom: '2%' }}>
-                                Annotations
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            {/* SQ TODO: Make this table scrollable for when it is larger than the screen */}
-                            <div style={{ overflowX: 'auto' }}>
-                            <TableContainer sx={{ overflowX: 'auto' }}>
+        <Grid container>
+            <Grid item xs={12}>
+                <TableContainer >
+
+                    {variantAnnotations.map(geneAnnotations => {
+                        return (
+                            <Card key={geneAnnotations.gene}>
+                                <h3>{geneAnnotations.gene}</h3>
+                                {geneAnnotations.transcripts && geneAnnotations.transcripts.length > 0 ?
+                                    <TranscriptsAnnotationsTable transcripts={geneAnnotations.transcripts} gene={geneAnnotations.gene} /> : <></>}
+                            </Card>
+
+                        )
+                    })}
+                    {/*}
                                 <Table aria-label="simple table" sx={{ minWidth: 800 }}>
                                     <colgroup>
                                         <col style={{ width: '10%'}}/>
@@ -49,7 +91,6 @@ export default function Annotations({variantAnnotations}) {
                                         <col style={{ width: '10%'}}/>
                                     </colgroup>
                                     <TableHead>
-                                        {/* BH TODO: Change the font of the headings in this table */}
                                         <TableRow>
                                             <TableCell align="center" sx={{ borderBottom: 'none', fontWeight: 'bold' }}>Gene</TableCell>
                                             <TableCell align="center" sx={{ borderBottom: 'none', fontWeight: 'bold' }}>Transcript</TableCell>
@@ -75,9 +116,6 @@ export default function Annotations({variantAnnotations}) {
                                                 </TableCell>
                                             </TableRow>
                                             {variantAnnotations[key].map((annotation, index) => (
-                                                /* BH TODO: Add error checking here for when there is no annotation data */
-                                                /* BH TODO: Add CADD and SpliceAI to API endpoint */
-                                                /* BH TODO: What falls under "Other Annotations" ? */
                                                 <TableRow key={index}>
                                                     <TableCell align="center">{annotation.transcript.variant_transcript.transcript.gene.short_name}</TableCell>
                                                     <TableCell align="center">{annotation.transcript.variant_transcript.transcript.transcript_id}</TableCell>
@@ -93,35 +131,10 @@ export default function Annotations({variantAnnotations}) {
                                                 </TableRow>
                                             ))}
                                         </TableBody>
-                                    ))}
-                                </Table>
-                            </TableContainer>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-            </Card>
-
-            <div style={{ marginTop: '20px' }} />
-            
-            <Card>
-                <CardContent>
-                    <Typography variant="h4" sx={{ fontWeight: 'light', paddingBottom: '2%' }}>
-                        Database
-                    </Typography>
-                    <RadioGroup
-                        row
-                        aria-label="transcripts"
-                        name="transcripts"
-                        value={selectedTranscript}
-                        onChange={handleTranscriptChange}
-                    >
-                        <FormControlLabel value="Ensembl" control={<Radio />} label="Ensembl" />
-                        <FormControlLabel value="Refseq" control={<Radio />} label="Refseq" />
-                    </RadioGroup>
-                </CardContent>
-            </Card>
-
-        </React.Fragment>
+                                    ))} 
+                                </Table>{*/}
+                </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
