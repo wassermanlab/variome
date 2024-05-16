7ed94438c789032b47297a2cad6b78724f9056de
Brad W <bwinter@cmmt.ubc.ca>
2024-05-16 16:50:13 -0700

    delete faster

--- ibvl/management/commands/import_ibvl.py
+++ ibvl/management/commands/import_ibvl.py
@@ -4,6 +4,7 @@ from django.core import serializers
 from django.apps import apps
 import os
 import datetime
+from django.db import connection
 from django.core.management import call_command
 import data.import_script.orchestrate as import_orchestrate
 from ibvl.models import Gene, GenomicGnomadFrequency, GenomicVariomeFrequency, Severity, SNV, Transcript, VariantAnnotation, VariantConsequence, VariantTranscript, Variant
@@ -11,21 +12,27 @@ from ibvl.models import Gene, GenomicGnomadFrequency, GenomicVariomeFrequency, S
 class Command(BaseCommand):
     help = 'deletes everything, then imports background variant data as per .env config'
     
-    
+        
 
     def handle(self, *args, **options):
+            
+        def truncate_table(model):
+            print("truncating table: ", model._meta.db_table)
+            with connection.cursor() as cursor:
+                cursor.execute('TRUNCATE TABLE {} CASCADE'.format(model._meta.db_table))
+                
+        truncate_table(Gene)
+        truncate_table(GenomicGnomadFrequency)
+        truncate_table(GenomicVariomeFrequency)
+        truncate_table(Severity)
+        truncate_table(SNV)
+        truncate_table(Transcript)
+        truncate_table(VariantAnnotation)
+        truncate_table(VariantConsequence)
+        truncate_table(VariantTranscript)
+        truncate_table(Variant)
         
-        Gene.objects.all().delete()
-        GenomicGnomadFrequency.objects.all().delete()
-        GenomicVariomeFrequency.objects.all().delete()
-        Severity.objects.all().delete()
-        SNV.objects.all().delete()
-        Transcript.objects.all().delete()
-        VariantAnnotation.objects.all().delete()
-        VariantConsequence.objects.all().delete()
-        VariantTranscript.objects.all().delete()
-        Variant.objects.all().delete()
-        
+        print("tables are empty. Now will import new data...")
         import_orchestrate.setup_and_run()
         print("done ")
         
