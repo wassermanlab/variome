dcfff9ec9a16b60b6b2d542e59e5c305aa2ed720
Brad W <bradw@example.com>
2024-05-28 12:59:58 -0700

    vit config uses ../.env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
