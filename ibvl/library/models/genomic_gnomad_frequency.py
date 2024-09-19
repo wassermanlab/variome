ea72b1e1445e928b5c2b4288a10bbddbc3b3559d
Brad W <bradw@example.com>
2024-09-19 16:31:54 -0700

    cleanup freq models; integers

--- ibvl/library/models/genomic_gnomad_frequency.py
+++ ibvl/library/models/genomic_gnomad_frequency.py
@@ -5,10 +5,9 @@ from .variant import Variant
 class GenomicGnomadFrequency(models.Model):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE, db_column='variant')
     af_tot = models.DecimalField(decimal_places=10, max_digits=12)
-    af_popmax = models.DecimalField(decimal_places=10, max_digits=12, null=True)
-    ac_tot = models.DecimalField(decimal_places=3, max_digits=12)
-    an_tot = models.DecimalField(decimal_places=3, max_digits=12)
-    hom_tot = models.DecimalField(decimal_places=3, max_digits=12)
+    ac_tot = models.PositiveIntegerField()
+    an_tot = models.PositiveIntegerField()
+    hom_tot = models.PositiveIntegerField()
 
     class Meta:
         verbose_name_plural = 'Genomic Gnomad Frequencies'
--- ibvl/library/models/genomic_variome_frequency.py
+++ ibvl/library/models/genomic_variome_frequency.py
@@ -5,18 +5,12 @@ from .variant import Variant
 class GenomicVariomeFrequency(models.Model):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE, db_column='variant')
     af_tot = models.DecimalField(decimal_places=10, max_digits=12)
-    af_xy = models.DecimalField(decimal_places=10, max_digits=12, null=True)
-    af_xx = models.DecimalField(decimal_places=10, max_digits=12, null=True)
-    ac_tot = models.DecimalField(decimal_places=3, max_digits=12)
-    ac_xy = models.DecimalField(decimal_places=3, max_digits=12, null=True)
-    ac_xx = models.DecimalField(decimal_places=3, max_digits=12, null=True)
-    an_tot = models.DecimalField(decimal_places=3, max_digits=12, null=True)
-    an_xy = models.DecimalField(decimal_places=3, max_digits=12, null=True)
-    an_xx = models.DecimalField(decimal_places=3, max_digits=12, null=True)
-    hom_tot = models.DecimalField(decimal_places=3, max_digits=12)
-    hom_xy = models.DecimalField(decimal_places=3, max_digits=12)
-    hom_xx = models.DecimalField(decimal_places=3, max_digits=12)
-    quality = models.DecimalField(decimal_places=3, max_digits=12)
+    ac_tot = models.PositiveIntegerField()
+    an_tot = models.PositiveIntegerField()
+    hom_tot = models.PositiveIntegerField()
+    hom_xy = models.PositiveIntegerField()
+    hom_xx = models.PositiveIntegerField()
+    quality = models.IntegerField()
 
     class Meta:
         verbose_name_plural = 'Genomic Variome Frequencies'
