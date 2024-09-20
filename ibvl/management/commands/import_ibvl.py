6c5b9801fb035108ffe80531cf584af0c00b2e11
Nick Phillips <nwp@zepler.net>
2024-09-20 16:38:39 +1200

    ATM-1190: Replace import_ibvl with simpler code
    Initial aim to be able to be more readable, simpler. Hoping faster but
    that may have to wait for optimisations.

--- ibvl/management/commands/import_ibvl.py
+++ ibvl/management/commands/import_ibvl.py
@@ -1,102 +1,161 @@
-import os
-from django.core.management.base import BaseCommand
-from django.db import connection
-from io import StringIO
-import pandas as pd
+import logging
+import pprint
+import argparse
+import sys
+from pathlib import Path
 
-import data.import_script.orchestrate as import_orchestrate
-from ibvl.library.models import (
-    Gene,
-    GenomicGnomadFrequency,
-    GenomicVariomeFrequency,
-    Severity,
-    SNV,
-    Transcript,
-    VariantAnnotation,
-    VariantConsequence,
-    VariantTranscript,
-    Variant,
-)
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+import ibvl.library.models as ibvlmodels
+import ibvl.management.tools as ibvltools
+
+log = logging.getLogger("management")
 
 
 class Command(BaseCommand):
-    help = "deletes the variant library tables, then imports from tsv files as per .env config"
+    help = "Import IBVL data from tsv files"
+    enable_debug = False
+    verbose = False
 
-    def handle(self, *args, **options):
+    def log_errors(self, entity_type, errors):
+        if errors:
+            sys.stderr.write(f"\nERRORS ({len(errors)}) in {entity_type} load:\n")
+            for err in errors:
+                sys.stderr.write(f"* {err}\n")
 
-        def truncate_table(model):
-            print("truncating table: ", model._meta.db_table)
-            with connection.cursor() as cursor:
-                cursor.execute("TRUNCATE TABLE {} CASCADE".format(model._meta.db_table))
-
-        start_at_model = os.getenv("START_AT_MODEL")
-
-        if isinstance(start_at_model, str) and start_at_model != "":
-            print(
-                f"starting at model {start_at_model}. likely resuming a failed migration so do not delete tables first"
-            )
-        else:
-            truncate_table(Gene)
-            truncate_table(GenomicGnomadFrequency)
-            truncate_table(GenomicVariomeFrequency)
-            truncate_table(SNV)
-            truncate_table(Transcript)
-            truncate_table(VariantAnnotation)
-            truncate_table(VariantConsequence)
-            truncate_table(VariantTranscript)
-            truncate_table(Variant)
-            print("tables are empty. Now will import new data...")
-
-        truncate_table(Severity)
-        severities_csv = """1,1,transcript_ablation
-                            2,2,splice_acceptor_variant
-                            3,3,splice_donor_variant
-                            4,4,stop_gained
-                            5,5,frameshift_variant
-                            6,6,stop_lost
-                            7,7,start_lost
-                            8,8,transcript_amplification
-                            9,9,inframe_insertion
-                            10,10,inframe_deletion
-                            11,11,missense_variant
-                            12,12,protein_altering_variant
-                            13,13,regulatory_region_ablation
-                            14,14,splice_region_variant
-                            15,15,incomplete_terminal_codon_variant
-                            16,16,start_retained_variant
-                            17,17,stop_retained_variant
-                            18,18,synonymous_variant
-                            19,19,coding_sequence_variant
-                            20,20,mature_miRNA_variant
-                            21,21,5_prime_UTR_variant
-                            22,22,3_prime_UTR_variant
-                            23,23,non_coding_transcript_exon_variant
-                            24,24,intron_variant
-                            25,25,NMD_transcript_variant
-                            26,26,non_coding_transcript_variant
-                            27,27,upstream_gene_variant
-                            28,28,downstream_gene_variant
-                            29,29,TFBS_ablation
-                            30,30,TFBS_amplification
-                            31,31,TF_binding_site_variant
-                            32,32,regulatory_region_amplification
-                            33,33,feature_elongation
-                            34,34,regulatory_region_variant
-                            35,35,feature_truncation
-                            36,36,intergenic_variant
-                            """
-
-        severities_df = pd.read_csv(
-            StringIO(severities_csv), names=["id", "severity_number", "consequence"]
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--path", "-p", dest="path", required=True,
+            help="Path of folder containing unpacked IBVL data files",
+        )
+        parser.add_argument(
+            "--severities", default=True, action=argparse.BooleanOptionalAction,
+            help="Import severities from supplied data?",
+        )
+        parser.add_argument(
+            "--genes", default=True, action=argparse.BooleanOptionalAction,
+            help="Import genes from supplied data?",
+        )
+        parser.add_argument(
+            "--variants", default=True, action=argparse.BooleanOptionalAction,
+            help="Import variants from supplied data?",
+        )
+        parser.add_argument(
+            "--transcripts", default=True, action=argparse.BooleanOptionalAction,
+            help="Import transcripts from supplied data?",
+        )
+        parser.add_argument(
+            "--snvs", default=True, action=argparse.BooleanOptionalAction,
+            help="Import SNVs from supplied data?",
+        )
+        parser.add_argument(
+            "--gvfs", default=True, action=argparse.BooleanOptionalAction,
+            help="Import GVFs from supplied data?",
+        )
+        parser.add_argument(
+            "--ggfs", default=True, action=argparse.BooleanOptionalAction,
+            help="Import GGFs from supplied data?",
+        )
+        parser.add_argument(
+            "--annotations", default=True, action=argparse.BooleanOptionalAction,
+            help="Import variant annotations from supplied data?",
+        )
+        parser.add_argument(
+            "--consequences", default=True, action=argparse.BooleanOptionalAction,
+            help="Import variant consequences from supplied data?",
+        )
+        parser.add_argument(
+            "--vts", default=True, action=argparse.BooleanOptionalAction,
+            help="Import variant transcripts from supplied data?",
+        )
+        parser.add_argument(
+            "--progress", default=True, action=argparse.BooleanOptionalAction,
+            help="Show progress indications?",
+        )
+        parser.add_argument(
+            "--failfast", default=False, action=argparse.BooleanOptionalAction,
+            help="Fail on first error?",
+        )
+        parser.add_argument(
+            "--rollback-on-error", "-r", dest="rollback-on-error", action="store_true", default=False,
+            help="Rollback (don't commit) database changes on errors",
+        )
+        parser.add_argument(
+            "--dry-run", "-n", dest="dry-run", action="store_true", default=False,
+            help="Dry run - make no changes to database",
         )
 
-        for severity in severities_df.iterrows():
-            Severity.objects.create(
-                id=severity[1]["id"],
-                severity_number=severity[1]["severity_number"],
-                consequence=severity[1]["consequence"],
-            )
-        print("re-imported severities")
+    def handle(self, **options):
+        transaction.set_autocommit(False)
+        log.debug("import_ibvl got options: \n %s", pprint.pformat(options))
 
-        import_orchestrate.setup_and_run()
-        print("done ")
+        sev_errors = None
+        gen_errors = None
+        var_errors = None
+        tra_errors = None
+        snv_errors = None
+        gvf_errors = None
+        ggf_errors = None
+        ann_errors = None
+        con_errors = None
+        vts_errors = None
+
+        importer = ibvltools.Importer(options)
+
+        if options['severities']:
+            sev_errors = importer.import_severities()
+
+        if options['genes']:
+            gen_errors = importer.import_genes()
+
+        if options['variants']:
+            var_errors = importer.import_variants()
+
+        if options['transcripts']:
+            tra_errors = importer.import_transcripts()
+
+        if options['snvs']:
+            tra_errors = importer.import_snvs()
+
+        if options['gvfs']:
+            gvf_errors = importer.import_gvfs()
+
+        if options['ggfs']:
+            ggf_errors = importer.import_ggfs()
+
+        if options['annotations']:
+            ann_errors = importer.import_annotations()
+
+        if options['consequences']:
+            con_errors = importer.import_consequences()
+
+        if options['vts']:
+            vts_errors = importer.import_vts()
+
+        self.log_errors("Severity", sev_errors)
+        self.log_errors("Gene", gen_errors)
+        self.log_errors("Variant", var_errors)
+        self.log_errors("Transcript", tra_errors)
+        self.log_errors("SNV", snv_errors)
+        self.log_errors("GVF", gvf_errors)
+        self.log_errors("GGF", ggf_errors)
+        self.log_errors("Annotation", ann_errors)
+        self.log_errors("Consequence", con_errors)
+        self.log_errors("Variant Transcript", vts_errors)
+
+        if options["rollback-on-error"]:
+            if (
+                sev_errors or gen_errors or var_errors or tra_errors or snv_errors or
+                gvf_errors or ggf_errors or ann_errors or con_errors or vts_errors
+            ):
+                sys.stderr.write("\nNot committing transaction (errors)\n")
+                transaction.rollback()
+                return
+
+        # if dry run, rollback transaction
+        if options["dry-run"]:
+            sys.stderr.write("\nNot committing transaction (dry-run)\n")
+            transaction.rollback()
+        else:
+            transaction.commit()
---
+++ ibvl/management/tools.py
@@ -0,0 +1,334 @@
+import logging
+import csv
+import sys
+from pathlib import Path
+
+import ibvl.library.models as ibvlmodels
+
+log = logging.getLogger("management")
+
+
+class IBVLDialect(csv.excel_tab):
+    pass
+
+
+class Importer:
+    def __init__(self, options):
+        self.path = Path(options["path"])
+        self.progress = options["progress"]
+        self.failfast = options["failfast"]
+
+    def import_severities(self):
+        """ Locate the severities data file and load severities from it """
+        errors = []
+        input_file = self.path / "severities.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Severity {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.Severity.objects.update_or_create(
+                            severity_number=row['severity_number'],
+                            defaults={
+                                "consequence": row['consequence'],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating severity {row['severity_number']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+    def import_genes(self):
+        """ Locate the genes data file and load genes from it """
+        errors = []
+        input_file = self.path / "genes" / "genes.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Gene {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.Gene.objects.update_or_create(
+                            short_name=row["short_name"],
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating gene {row['short_name']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+    def import_variants(self):
+        """ Locate the variants data file and load variants from it """
+        errors = []
+        input_file = self.path / "variants" / "variants.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Variant {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.Variant.objects.update_or_create(
+                            variant_id=row["variant_id"],
+                            defaults={
+                                "var_type": row["var_type"],
+                                "filter": row["filter"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating variant {row['variant_id']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+    def import_transcripts(self):
+        """ Locate the transcripts data file and load transcripts from it """
+        errors = []
+        input_file = self.path / "transcripts" / "transcripts.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Transcript {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.Transcript.objects.update_or_create(
+                            transcript_id=row["transcript_id"],
+                            defaults={
+                                "gene": ibvlmodels.Gene.objects.get(short_name=row["gene"]),
+                                "transcript_type": row["transcript_type"],
+                                "tsl": row["tsl"],
+                                "biotype": row["biotype"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating transcript {row['transcript_id']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+    def import_snvs(self):
+        """ Locate the SNVs data file and load SNVs from it """
+        errors = []
+        input_file = self.path / "snvs" / "snvs.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"SNV {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.SNV.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            defaults={
+                                "type": row["type"],
+                                "length": row["length"],
+                                "chr": row["chr"],
+                                "pos": row["pos"],
+                                "ref": row["ref"],
+                                "alt": row["alt"],
+                                "cadd_intr": row["cadd_intr"],
+                                "cadd_score": row["cadd_score"],
+                                "dbsnp_url": row["dbsnp_url"],
+                                "dbsnp_id": row["dbsnp_id"],
+                                "ucsc_url": row["ucsc_url"],
+                                "ensembl_url": row["ensembl_url"],
+                                "clinvar_vcv": row["clinvar_vcv"],
+                                "clinvar_url": row["clinvar_url"],
+                                "gnomad_url": row["gnomad_url"],
+                                "splice_ai": row["splice_ai"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating SNV for variant {row['variant']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+
+    def import_gvfs(self):
+        """ Locate the GVFs data file and load GVFs from it """
+        errors = []
+        input_file = self.path / "genomic_variome_frequencies" / "genomic_variome_frequencies.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"GVF {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.GenomicVariomeFrequency.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            defaults={
+                                "af_tot": row["af_tot"],
+                                "ac_tot": row["ac_tot"],
+                                "an_tot": row["an_tot"],
+                                "hom_tot": row["hom_tot"],
+                                "hom_xx": row["hom_xx"],
+                                "hom_xy": row["hom_xy"],
+                                "quality": row["quality"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating GVF for variant {row['variant']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+
+    def import_ggfs(self):
+        """ Locate the GGFs data file and load GGFs from it """
+        errors = []
+        input_file = self.path / "genomic_gnomad_frequencies" / "genomic_gnomad_frequencies.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"GGF {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.GenomicGnomadFrequency.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            defaults={
+                                "af_tot": row["af_tot"],
+                                "ac_tot": row["ac_tot"],
+                                "an_tot": row["an_tot"],
+                                "hom_tot": row["hom_tot"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating GGF for variant {row['variant']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+
+    def import_annotations(self):
+        """ Locate the annotations data file and load annotations from it """
+        errors = []
+        input_file = self.path / "variants_annotations" / "variants_annotations.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Annotation {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.VariantAnnotation.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            transcript=ibvlmodels.Transcript.objects.get(transcript_id=row["transcript"]),
+                            defaults={
+                                "hgvsc": row["hgvsc"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating annotation for variant {row['variant']} transcript {row['transcript']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+
+    def import_consequences(self):
+        """ Locate the consequences data file and load consequences from it """
+        errors = []
+        input_file = self.path / "variants_consequences" / "variants_consequences.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"Consequence {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.VariantAnnotation.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            transcript=ibvlmodels.Transcript.objects.get(transcript_id=row["transcript"]),
+                            defaults={
+                                "severity": ibvlmodels.Severity.objects.get(row["severity"]),
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating consequence for variant {row['variant']} transcript {row['transcript']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
+
+    def import_vts(self):
+        """ Locate the variant transcripts data file and load VTs from it """
+        errors = []
+        input_file = self.path / "variants_transcripts" / "variants_transcripts.tsv"
+        with open(input_file, newline="") as f:
+            reader = csv.DictReader(f, dialect=IBVLDialect)
+            try:
+                for row in reader:
+                    if self.progress and reader.line_num % 1000 == 0:
+                        sys.stderr.write(f"VT {reader.line_num}...\n")
+                    try:
+                        obj, created = ibvlmodels.VariantTranscript.objects.update_or_create(
+                            variant=ibvlmodels.Variant.objects.get(variant_id=row["variant"]),
+                            transcript=ibvlmodels.Transcript.objects.get(transcript_id=row["transcript"]),
+                            defaults={
+                                "hgvsc": row["hgvsc"],
+                            },
+                        )
+                    except Exception as e:
+                        msg = f"error creating/updating VT for variant {row['variant']} transcript {row['transcript']} from line {reader.line_num}: {e}"
+                        errors.append(msg)
+                        if self.failfast:
+                            return errors
+            except csv.Error as e:
+                errors.append(f"error reading line {reader.line_num}: {e}")
+                if self.failfast:
+                    return errors
+        return errors
+
