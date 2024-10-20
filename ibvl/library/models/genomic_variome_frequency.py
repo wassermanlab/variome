109586dae607ec170c93b523a0c1b62456e97b92
Brad W <bradw@example.com>
2024-10-20 16:40:20 -0700

    almost all nullable in gvf

--- ibvl/library/models/genomic_variome_frequency.py
+++ ibvl/library/models/genomic_variome_frequency.py
@@ -7,16 +7,16 @@ class GenomicVariomeFrequency(models.Model):
     af_tot = models.DecimalField(decimal_places=10, max_digits=12, null=True)
     af_xx = models.DecimalField(decimal_places=10, max_digits=12, null=True)
     af_xy = models.DecimalField(decimal_places=10, max_digits=12, null=True)
-    ac_tot = models.PositiveIntegerField()
+    ac_tot = models.PositiveIntegerField(null=True)
     an_tot = models.PositiveIntegerField()
     ac_xx = models.PositiveIntegerField(null=True)
     ac_xy = models.PositiveIntegerField(null=True)
     an_xx = models.PositiveIntegerField(null=True)
     an_xy = models.PositiveIntegerField(null=True)
-    hom_tot = models.PositiveIntegerField()
+    hom_tot = models.PositiveIntegerField(null=True)
     hom_xy = models.PositiveIntegerField(null=True)
     hom_xx = models.PositiveIntegerField(null=True)
-    quality = models.IntegerField()
+    quality = models.IntegerField(null=True)
 
     class Meta:
         verbose_name_plural = 'Genomic Variome Frequencies'
