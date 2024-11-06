e579de596b78de8e447406e7ef062477e1206071
Brad W <bradw@example.com>
2024-11-05 16:37:08 -0800

    consolidate migrations

--- ibvl/models/variome_settings.py
+++ ibvl/models/variome_settings.py
@@ -4,7 +4,8 @@ from ibvl.library.models.variant import Variant
 class VariomeSettings(models.Model):
     """Singleton container for user-editable (Admin) settings and config."""
     site_title = models.CharField(max_length=255) 
-    example_snv = models.ForeignKey(Variant, on_delete=models.CASCADE, null=True)
+    home_page_message = models.TextField(blank=True, default="This is a test database. All data used is open source and does not include Indigenous data.")
+    example_snv = models.ForeignKey(Variant, on_delete=models.CASCADE, null=True, blank=True)
 #    example_mt = models.ForeignKey('library.snv', on_delete=models.CASCADE, blank=True, null=True)
 #    example_sv = models.ForeignKey('library.snv', on_delete=models.CASCADE, blank=True, null=True)
     
