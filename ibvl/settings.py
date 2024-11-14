1985f30d22d08ff851639d5214200ba46a99eeb9
bradwbradw <braddjwinter@gmail.com>
2024-11-13 17:10:32 -0800

    rm dupe line and superfluous admin.py file

--- ibvl/settings.py
+++ ibvl/settings.py
@@ -183,7 +183,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.request",
                 "ibvl.context_processors.custom_admin_dashboard",
             ],
         },
